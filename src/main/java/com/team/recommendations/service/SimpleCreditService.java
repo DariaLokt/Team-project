0116a576ef66fe55def96c5b75f6155176b7b0c6
DariaLokt <d.lokteeva@yandex.ru>
2025-10-08 23:07:32 +0500

    Services

--- src/main/java/com/team/recommendations/service/SimpleCreditService.java
+++ src/main/java/com/team/recommendations/service/SimpleCreditService.java
@@ -1,4 +1,49 @@
 package com.team.recommendations.service;
 
-public class SimpleCreditService {
+import com.team.recommendations.model.Recommendation;
+import com.team.recommendations.repository.RecommendationsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class SimpleCreditService implements RuleSetService {
+    private final RecommendationsRepository recommendationsRepository;
+
+    public SimpleCreditService(RecommendationsRepository recommendationsRepository) {
+        this.recommendationsRepository = recommendationsRepository;
+    }
+
+    @Override
+    public Optional<Recommendation> getRecommendation(UUID id) {
+        Recommendation recommendation = null;
+        if (isGettingRecommendation(id)) {
+            recommendation = new Recommendation("Простой кредит", UUID.fromString("ab138afb-f3ba-4a93-b74f-0fcee86d447f"),
+                    "Откройте мир выгодных кредитов с нами!\n" +
+                            "\n" +
+                            "Ищете способ быстро и без лишних хлопот получить нужную сумму? Тогда наш выгодный кредит — именно то, что вам нужно! Мы предлагаем низкие процентные ставки, гибкие условия и индивидуальный подход к каждому клиенту.\n" +
+                            "\n" +
+                            "Почему выбирают нас:\n" +
+                            "\n" +
+                            "Быстрое рассмотрение заявки. Мы ценим ваше время, поэтому процесс рассмотрения заявки занимает всего несколько часов.\n" +
+                            "\n" +
+                            "Удобное оформление. Подать заявку на кредит можно онлайн на нашем сайте или в мобильном приложении.\n" +
+                            "\n" +
+                            "Широкий выбор кредитных продуктов. Мы предлагаем кредиты на различные цели: покупку недвижимости, автомобиля, образование, лечение и многое другое.\n" +
+                            "\n" +
+                            "Не упустите возможность воспользоваться выгодными условиями кредитования от нашей компании!");
+        }
+        return Optional.ofNullable(recommendation);
+    }
+
+    public boolean isGettingRecommendation(UUID id) {
+//        rule1
+
+//        rule2
+
+//        rule3
+
+        return true;
+    }
 }
--- src/main/java/com/team/recommendations/service/TopSavingService.java
+++ src/main/java/com/team/recommendations/service/TopSavingService.java
@@ -1,4 +1,47 @@
 package com.team.recommendations.service;
 
-public class TopSavingService {
+import com.team.recommendations.model.Recommendation;
+import com.team.recommendations.repository.RecommendationsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TopSavingService implements RuleSetService {
+    private final RecommendationsRepository recommendationsRepository;
+
+    public TopSavingService(RecommendationsRepository recommendationsRepository) {
+        this.recommendationsRepository = recommendationsRepository;
+    }
+
+    @Override
+    public Optional<Recommendation> getRecommendation(UUID id) {
+        Recommendation recommendation = null;
+        if (isGettingRecommendation(id)) {
+            recommendation = new Recommendation("Top Saving", UUID.fromString("59efc529-2fff-41af-baff-90ccd7402925"),
+                    "Откройте свою собственную «Копилку» с нашим банком! «Копилка» — это уникальный банковский инструмент, который поможет вам легко и удобно накапливать деньги на важные цели. Больше никаких забытых чеков и потерянных квитанций — всё под контролем!\n" +
+                            "\n" +
+                            "Преимущества «Копилки»:\n" +
+                            "\n" +
+                            "Накопление средств на конкретные цели. Установите лимит и срок накопления, и банк будет автоматически переводить определенную сумму на ваш счет.\n" +
+                            "\n" +
+                            "Прозрачность и контроль. Отслеживайте свои доходы и расходы, контролируйте процесс накопления и корректируйте стратегию при необходимости.\n" +
+                            "\n" +
+                            "Безопасность и надежность. Ваши средства находятся под защитой банка, а доступ к ним возможен только через мобильное приложение или интернет-банкинг.\n" +
+                            "\n" +
+                            "Начните использовать «Копилку» уже сегодня и станьте ближе к своим финансовым целям!");
+        }
+        return Optional.ofNullable(recommendation);
+    }
+
+    public boolean isGettingRecommendation(UUID id) {
+//        rule1
+
+//        rule2
+
+//        rule3
+
+        return true;
+    }
 }
