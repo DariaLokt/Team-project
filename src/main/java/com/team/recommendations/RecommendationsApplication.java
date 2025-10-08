25ea5caf2a9950df2887f13d9010c8c4bc3c8cb6
DariaLokt <d.lokteeva@yandex.ru>
2025-10-08 21:35:52 +0500

    JDBC

---
+++ src/main/java/com/team/recommendations/RecommendationsApplication.java
@@ -0,0 +1,13 @@
+package com.team.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationsApplication.class, args);
+	}
+
+}
