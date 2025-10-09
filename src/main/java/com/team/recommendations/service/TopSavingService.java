dcd8b42b1434311bfc92c4530cee42a94cf57275
DariaLokt <d.lokteeva@yandex.ru>
2025-10-09 22:30:00 +0500

    Теперь в репозитории многофункциональные функции

--- src/main/java/com/team/recommendations/service/TopSavingService.java
+++ src/main/java/com/team/recommendations/service/TopSavingService.java
@@ -39,12 +39,12 @@ public class TopSavingService implements RuleSetService {
 
     public boolean isGettingRecommendation(UUID id) {
 //        rule1
-        IfUsedRule rule1 = new IfUsedRule(recommendationsRepository.getDebitUse(id),true);
+        IfUsedRule rule1 = new IfUsedRule(recommendationsRepository.getUse(id,"DEBIT"),true);
 //        rule2
-        CompareRule rule2_1 = new CompareRule(recommendationsRepository.getDebitDeposit(id),">=",50000);
-        CompareRule rule2_2 = new CompareRule(recommendationsRepository.getSavingDeposit(id),">=",50000);
+        CompareRule rule2_1 = new CompareRule(recommendationsRepository.getTransactionSum(id,"DEBIT","DEPOSIT"),">=",50000);
+        CompareRule rule2_2 = new CompareRule(recommendationsRepository.getTransactionSum(id,"SAVING","DEPOSIT"),">=",50000);
 //        rule3
-        CompareRule rule3 = new CompareRule(recommendationsRepository.getDebitDeposit(id),">",recommendationsRepository.getDebitWithdraw(id));
+        CompareRule rule3 = new CompareRule(recommendationsRepository.getTransactionSum(id,"DEBIT","DEPOSIT"),">",recommendationsRepository.getTransactionSum(id,"DEBIT","WITHDRAW"));
 
         return rule1.isFollowed() && (rule2_1.isFollowed() || rule2_2.isFollowed()) && rule3.isFollowed();
     }
