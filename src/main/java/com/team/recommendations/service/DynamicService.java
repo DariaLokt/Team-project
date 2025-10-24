17906ea12bcea5e890fc5e2c61737c7868d45f68
DariaLokt <d.lokteeva@yandex.ru>
2025-10-24 12:52:10 +0500

    Добавила кеширование и логирование

--- src/main/java/com/team/recommendations/service/DynamicService.java
+++ src/main/java/com/team/recommendations/service/DynamicService.java
@@ -4,6 +4,8 @@ import com.team.recommendations.model.dynamic.DynamicProduct;
 import com.team.recommendations.model.dynamic.DynamicRule;
 import com.team.recommendations.repository.DynamicProductRepository;
 import com.team.recommendations.repository.DynamicRuleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -14,12 +16,15 @@ public class DynamicService {
     private final DynamicProductRepository dynamicProductRepository;
     private final DynamicRuleRepository dynamicRuleRepository;
 
+    Logger logger = LoggerFactory.getLogger(DynamicService.class);
+
     public DynamicService(DynamicProductRepository dynamicProductRepository, DynamicRuleRepository dynamicRuleRepository) {
         this.dynamicProductRepository = dynamicProductRepository;
         this.dynamicRuleRepository = dynamicRuleRepository;
     }
 
     public DynamicProduct addProduct(DynamicProduct product) {
+        logger.info("The method for adding a product was invoked");
         Collection<DynamicRule> newRules = product.getRule();
         dynamicRuleRepository.saveAll(newRules);
         dynamicRuleRepository.findAll().stream()
@@ -29,10 +34,12 @@ public class DynamicService {
     }
 
     public Collection<DynamicProduct> getAllProducts() {
+        logger.info("The method for getting all the products was invoked");
         return dynamicProductRepository.findAll();
     }
 
-    public void deleteProduct(UUID id){
+    public void deleteProduct(UUID id) {
+        logger.info("The method for deleting a product was invoked");
         DynamicProduct product = dynamicProductRepository.findById(id).orElseThrow();
         dynamicRuleRepository.deleteAll(product.getRule());
         dynamicProductRepository.delete(product);
--- src/main/java/com/team/recommendations/service/Invest500Service.java
+++ src/main/java/com/team/recommendations/service/Invest500Service.java
@@ -4,6 +4,8 @@ import com.team.recommendations.model.recommendations.Recommendation;
 import com.team.recommendations.model.rules.CompareRule;
 import com.team.recommendations.model.rules.IfUsedRule;
 import com.team.recommendations.repository.RecommendationsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,8 +19,11 @@ public class Invest500Service implements RuleSetService{
         this.recommendationsRepository = recommendationsRepository;
     }
 
+    Logger logger = LoggerFactory.getLogger(Invest500Service.class);
+
     @Override
     public Optional<Recommendation> getRecommendation(UUID id) {
+        logger.info("Adding Invest500 function was invoked for user: {}", id);
         Recommendation recommendation = null;
         if (isGettingRecommendation(id)) {
             recommendation = new Recommendation("Invest 500", UUID.fromString("147f6a0f-3b91-413b-ab99-87f081d60d5a"), "Откройте свой путь к успеху с индивидуальным инвестиционным счетом (ИИС) от нашего банка! Воспользуйтесь налоговыми льготами и начните инвестировать с умом. Пополните счет до конца года и получите выгоду в виде вычета на взнос в следующем налоговом периоде. Не упустите возможность разнообразить свой портфель, снизить риски и следить за актуальными рыночными тенденциями. Откройте ИИС сегодня и станьте ближе к финансовой независимости!");
@@ -34,6 +39,8 @@ public class Invest500Service implements RuleSetService{
         IfUsedRule rule2 = new IfUsedRule(recommendationsRepository.getUse(id,"INVEST"), false);
 //        rule3
         CompareRule rule3 = new CompareRule(recommendationsRepository.getTransactionSum(id,"SAVING","DEPOSIT"),">",1000);
+
+        logger.info("Invest500 was checked for user: {}", id);
 
         return rule1.isFollowed() && rule2.isFollowed() && rule3.isFollowed();
     }
