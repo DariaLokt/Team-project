7468c76f22e4a7651dc71a630c55c30c7e1a914d
DariaLokt <d.lokteeva@yandex.ru>
2025-11-02 18:00:00 +0500

    Сделала вывод информации о сервисе

--- src/main/java/com/team/recommendations/controller/ManagementController.java
+++ src/main/java/com/team/recommendations/controller/ManagementController.java
@@ -1,7 +1,11 @@
 package com.team.recommendations.controller;
 
+import com.team.recommendations.model.ServiceInfo;
 import com.team.recommendations.service.CacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ManagementController {
     private final CacheService cacheService;
 
+    @Autowired
+    private BuildProperties buildProperties;
+
     public ManagementController(CacheService cacheService) {
         this.cacheService = cacheService;
     }
@@ -19,5 +26,13 @@ public class ManagementController {
     public ResponseEntity<String> clearCache() {
         cacheService.evictAllCaches();
         return ResponseEntity.ok("Кэш сброшен");
+    }
+
+    @GetMapping("/info")
+    public ServiceInfo getServiceInfo() {
+        return new ServiceInfo(
+                buildProperties.getArtifact(),
+                buildProperties.getVersion()
+        );
     }
 }
